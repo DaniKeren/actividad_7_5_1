e0f6643d322920c0e435de75b783ebd2254813b1
KerenMedina; <kerenmedinacoasta@gmail.com>
2019-02-07 10:32:21 +0100

    Hola Mundo 3

--- actividad7_5_1/src/actividad7_5_1/Actividad7_5_1.java
+++ actividad7_5_1/src/actividad7_5_1/Actividad7_5_1.java
@@ -15,7 +15,7 @@ public class Actividad7_5_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Adios");
+        System.out.println("Hola Mundo 3");
     }
     
 }
